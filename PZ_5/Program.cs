539b6c1fc6ec59de9ee1ba48c9e74b5662660b91
Am1rggwp <91305537+Am1rggwp@users.noreply.github.com>
2023-10-12 22:12:08 +0500

    Press "F"

--- PZ_5/Program.cs
+++ PZ_5/Program.cs
@@ -5,13 +5,14 @@ class Program
 {
     static object lockObject = new object();
     static bool isThread2Blocked = false;
+    static int pauseVal = 50;
     static int criticalValue = 100;
     int value = 2;
     
     static void Main(string[] args)
     {
         Program p = new Program();
-        
+
         Thread thread1 = new Thread(p.Thread1Method);
         Thread thread2 = new Thread(p.Thread2Method);
 
@@ -19,41 +20,41 @@ class Program
         thread2.Start();
     }
 
-    
-
-     public void Thread1Method ()
+    public void Thread1Method ()
     {
         while (true)
         {
-
             Thread.Sleep(1200);
             Console.WriteLine("Поток 1: Получено из потока 2: " + value);
 
-            if (value >= criticalValue)
-            {
-                Console.WriteLine("Поток 1: Достигнуто критическое значение. Остановлен Поток 2 .");
-                StopThread2();
-                break;
-            }
+                if (value >= criticalValue)
+                {
+
+                    Console.WriteLine("Остановка Потока 2.");
+                    StopThread2();
+                    break;
+                }
+          
+
+            
         }
-     }
-    
-     void Thread2Method()
+    }
+
+    void Thread2Method()
     {
         while (true)
         {
             value *= 2;
             Thread.Sleep(1000);
 
-            if (value >= criticalValue)
-            {
-                Console.WriteLine("Остановка потока 1.");
-                StopThread1();
-                break;
-            }
+             if (value >= criticalValue)
+             {
+                 Console.WriteLine("Остановка потока 1.");
+                 StopThread1();
+                 break;
+             }
         }
     }
-
     static void StopThread1()
     {
         lock (lockObject)
---
+++ PZ_6/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Enumeration;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using static PZ_6.MainPage;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace PZ_6
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        internal class PageInfo
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        private void PageOneClick(object sender, RoutedEventArgs e)
+        {
+            PageInfo pageInfo = new PageInfo { Id = 001, Name = "Ты попал на страницу №1" };
+            Frame.Navigate(typeof(Page1), pageInfo);
+        }
+
+        private void PageTwoClick(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Page2), "Переход из главного окна на страницу №2");
+
+        }
+    }
+}
