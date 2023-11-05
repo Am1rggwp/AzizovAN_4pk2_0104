5fcced12090994831382e9f7f3cf71d681ac83ba
Amir Azizov <amir.azizov2015@mail.ru>
2023-11-05 15:54:24 +0500

    Press "F"

---
+++ PZ_18/AddEl.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace PZ_18
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class AddEl : Page
+    {
+        public AddEl()
+        {
+            this.InitializeComponent();
+            //Button button1 = new Button();
+            //button1.Content = "Кнопка кода";
+            //button1.Width = 120;
+            //button1.Height = 40;
+
+            //layoutGrid.Children.Add(button1);
+        }
+    }
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+
+        public override string ToString()
+        {
+            return $"Cтудент {this.Name} из группы {this.Group}";
+        }
+    }
+
+}
+
---
+++ PZ_18/Btn.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PZ_18
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class Btn : Page
+    {
+        public Btn()
+        {
+            this.InitializeComponent();
+        }
+
+        private void bnt1_Click(object sender, RoutedEventArgs e)
+        {
+            btn1.Content = "❤";
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            btn2.Content = "Кто это ?";
+        }
+    }
+}
